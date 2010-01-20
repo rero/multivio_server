0a51a6283369a8e7da935f1cee5f827744eb9523
Johnny Mariéthoz <Johnny.Mariethoz@rero.ch>
2010-01-20 17:56:36 +0100

    Added default values for pdf metadata.

--- multivio/parser.py
+++ multivio/parser.py
@@ -156,10 +156,18 @@ class PdfParser(Parser):
         reader = pyPdf.PdfFileReader(stream)
         
         metadata = {}
-        metadata['title'] = reader.getDocumentInfo().title
-        metadata['creator'] = reader.getDocumentInfo().author
+        label=None
+        if reader.getDocumentInfo().title is not None:
+            metadata['title'] = reader.getDocumentInfo().title
+            label = metadata['title']
+        else:
+            metadata['title'] = 'unknown'
+        if reader.getDocumentInfo().author is not None:
+            metadata['creator'] = reader.getDocumentInfo().author
+        else:
+            metadata['creator'] = 'unknown'
         metadata['language'] = 'unknown'
-        root = self._cdm.addNode(metadata=metadata, label=metadata['title']) 
+        root = self._cdm.addNode(metadata=metadata, label=label) 
 
         i = 1
         for url in range(reader.getNumPages()):
