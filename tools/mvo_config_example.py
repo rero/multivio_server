d2499dae789d3d9e89e99ccddca8d263f506b8a4
Johnny Mariéthoz <Johnny.Mariethoz@rero.ch>
2010-02-03 16:28:43 +0100

    Added sample config file.

---
+++ tools/mvo_config_example.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = "Johnny Mariethoz <Johnny.Mariethoz@rero.ch>"
+__version__ = "0.0.0"
+__copyright__ = "Copyright (c) 2009 Rero, Johnny Mariethoz"
+__license__ = "Internal Use Only"
+
+
+
+class MVOConfig:
+
+    class General:
+        temp_dir = '/tmp' 
+
+    class Url:
+        user_agent = 'Firefox/3.5.2'
+
+
+
