d6bbd81783edff782402ced2e4351fd0b5a81c7f
jma <Johnny.Mariethoz@rero.ch>
2010-04-01 17:26:52 +0200

    Make the multivio dispatcher conformant to pylint.

--- multivio/dispatcher.py
+++ multivio/dispatcher.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
-
 # -*- coding: utf-8 -*-
+"""Multivio HTTP requests dispatcher."""
 
 __author__ = "Johnny Mariethoz <Johnny.Mariethoz@rero.ch>"
 __version__ = "0.0.0"
@@ -11,13 +11,12 @@ __license__ = "Internal Use Only"
 #---------------------------- Modules -----------------------------------------
 
 # import of standard modules
-import sys
-import os
 import re
 from optparse import OptionParser
 
 # third party modules
 import logger
+import logging
 import mvo_parser
 import document
 
@@ -33,7 +32,10 @@ class Dispatcher(Application):
     """
 
     def __init__(self):
+        "Simple constructor." 
         
+        Application.__init__(self)
+
         #application configuration
         self._apps = {}
         #Client logger
@@ -43,61 +45,58 @@ class Dispatcher(Application):
         self._apps['.*?/document/get'] = \
             document.DocumentApp(temp_dir=MVOConfig.General.temp_dir)
         self.usage = """<br><h1>Welcome to the multivio server.</h1><br>"""
+        self.logger = logging.getLogger(MVOConfig.Logger.name+".Dispatcher")
         
     def __call__(self, environ, start_response):
+        """Main method to dispatch HTTP requests."""
+
         (path, opts) = self.getParams(environ)
-        print "Request: %s", path
+        self.logger.debug("Request: %s", path)
         if re.match('.*?/help', path) or len(path) == 0:
             start_response('200 OK', [('content-type', 'text/html')])
             response = [self.usage]
             response.extend(["<h2>Available pathes:</h2>"])
             for k in self._apps.keys():
                 response.extend(["<h3>%s</h3>" % k])
-                print self._apps[k].usage
+                self.logger.debug(self._apps[k].usage)
                 response.extend([self._apps[k].usage])
             return response
         for k in self._apps.keys():
             if re.match(k, path):
                 return self._apps[k](environ, start_response)
         else:
-            start_response('404 File Not Found', [('content-type', 'text/html')])
+            start_response('404 File Not Found', [('content-type',
+                            'text/html')])
             return ["Invalid URL."]
-
-        #if re.match('.*?/log/post', path):
-        #    return self._logger_app(environ, start_response)
-
-        #if re.match('.*?/cdm/get', path):
-        #    return self._cdm_app(environ, start_response)
-        #
-        #if re.match('.*?/document/get', path):
-        #    return self._document_app(environ, start_response)
 
 
 #---------------------------- Main Part ---------------------------------------
 
-if __name__ == '__main__':
+def main():
+    """Main function"""
 
     usage = "usage: %prog [options]"
 
     parser = OptionParser(usage)
 
-    parser.set_description ("Change It")
-
-
+    parser.set_description ("Web app to test the dispatcher.")
 
     parser.add_option ("-v", "--verbose", dest="verbose",
                        help="Verbose mode",
                        action="store_true", default=False)
 
+    parser.add_option ("-p", "--port", dest="port",
+                       help="Http Port (Default: 4041)",
+                       type="int", default=4041)
 
-    (options,args) = parser.parse_args ()
+    (options, args) = parser.parse_args ()
 
     if len(args) != 0:
         parser.error("Error: incorrect number of arguments, try --help")
     from wsgiref.simple_server import make_server
     application = Dispatcher()
-    #server = make_server('localhost', 8081, simple_app)
-    server = make_server('', 4041, application)
+    server = make_server('', options.port, application)
     server.serve_forever()
 
-
+if __name__ == '__main__':
+    main()
