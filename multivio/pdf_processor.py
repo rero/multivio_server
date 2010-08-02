33922cb270866cab685d902ea2807ec3bf745810
root <root@mvo-server.localdomain>
2010-08-02 17:27:19 +0200

    added pdf search, w/ debug msgs. TODO: context words

--- multivio/pdf_processor.py
+++ multivio/pdf_processor.py
@@ -12,6 +12,7 @@ __license__ = "Internal Use Only"
 
 # import of standard modules
 import cStringIO
+import sys
 
 # local modules
 from processor import DocumentProcessor
@@ -78,8 +79,88 @@ class PdfProcessor(DocumentProcessor):
             sort -- string: sort the results given the sort criterion
         return:
             a dictionary with the founded results
-        """
-        return None
+        """    
+        # number of results
+        num_results = 0
+
+        # number of pages in document
+        num_pages = self._doc.getNumPages()        
+
+        print "%d pages detected" % num_pages
+    
+        # conversion of text to search to unicode
+        query = unicode(query.decode('utf-8'))
+
+        ## findText options: perform find from top to bottom of page
+        startAtTop = True
+        stopAtBottom = True
+        startAtLast = False
+        stopAtLast = False
+        caseSensitive = False
+        backward = False
+
+        # param check TODO report errors
+        if (from_ is None): from_ = 1
+        if (to_ is None): to_ = num_pages + 1   
+        if (max_results in [0, None]): max_results = sys.maxint
+
+        # calculate page range [1, num_pages+1]
+        (from_, to_) = (max(from_, 1), min(to_ + 1, num_pages + 1))
+
+        # results for the current file
+        result = {
+          'context': 'text',
+          'file_position': {
+            'url':'TODO://%s' % self._file_name,
+            'results':[ # results for that file go here
+            ] 
+          }
+        }
+
+        # current result
+        cur_res = {}
+
+        # note: to_ is not included in range
+        done = False
+        for np in range(from_, to_):
+            # debug
+            print "searching on page %d" % np
+
+            if done is True:
+                break
+
+            td = poppler.TextOutputDev(None, True, False, False)
+            self._doc.displayPage(td, np, 72, 72, 0, True, True, False)
+            text_page = td.takeText()
+            (found, x1, y1, x2, y2) = text_page.findText(query, startAtTop, stopAtBottom, startAtLast, stopAtLast, caseSensitive, backward)
+        
+            ## findText options: find from last find to bottom of page
+            startAtTop = False
+            stopAtBottom = True 
+            startAtLast = True
+  
+            while found is True:
+                #print "%s word found at page %s, bbx: (%s, %s, %s, %s)" % (query, np, x1, y1, x2, y2)
+                # build structure for the found word
+                cur_res = {
+                  'preview': 'TODO: %s'%query,
+                  'index': {
+                    'page': np, 
+                    'bounding_box': {'x1':x1, 'y1':y1, 'x2':x2, 'y2':y2}
+                  }
+                }
+                
+                # append result to list
+                result['file_position']['results'].append(cur_res)
+                                
+                num_results = num_results + 1
+                if (num_results >= max_results):
+                    done = True
+                    break
+                # find additional words on page
+                (found, x1, y1, x2, y2) = text_page.findText(query, startAtTop, stopAtBottom, startAtLast, stopAtLast, caseSensitive, backward)
+
+        return result
 
     def indexing(self, output_file):
         """Batch indexing of the document.
@@ -139,3 +220,11 @@ class PdfProcessor(DocumentProcessor):
         #str(len(content)))]
         return('image/jpeg', content)
 
+# TODO: test
+p = PdfProcessor('/examples/d05.pdf')
+r = p.search(query='alors', from_=2, to_=100, max_results=0, sort=None)
+
+if r:
+    print "# results: %s"%(len(r['file_position']['results']))
+else:
+    print r
