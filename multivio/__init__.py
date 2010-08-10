ed1715e724c17f0ec1cba8998e67bd1622bc6b52
jma <Johnny.Mariethoz@rero.ch>
2010-08-10 10:12:02 +0200

    Changed release version to devel.

--- multivio/__init__.py
+++ multivio/__init__.py
@@ -3,8 +3,7 @@ Multivio Python package.
 """
 
 __author__ = "Johnny Mariethoz <Johnny.Mariethoz@rero.ch>"
-__version__ = "0.2.1"
-#__version__ = "0.2.2010-08-05 16:57:01"
+__version__ = "0.2.2010-08-10 10:11:40"
 __copyright__ = "Copyright (c) 2009,2010 RERO, Johnny Mariethoz"
 __license__ = "Internal Use Only"
 
