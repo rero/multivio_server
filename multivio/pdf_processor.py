rero/multivio_server

commit 6df4e8e4c5eb7738ab04687b5a745eab560e33f3

Fixed issue with first line of a page in certain pdfs
* elements such as page number in footer sometimes
appear at the beginning of the word list (from poppler)
and disrupt line coordinates, making selection of said
line impossible due to incoherent coordinates in
page indexing.

multivio/pdf_processor.py
@@ -405,8 +405,8 @@ class PdfProcessor(DocumentProcessor):
         # get words' list for a page
         # TextOutputDev(char *fileName, GBool physLayoutA, 
         #               GBool rawOrderA, GBool append)
-        # NOTE: don't use physical layout, use raw order of words in the page
-        td = poppler.TextOutputDev(None, False, True, False)
+        # NOTE: use physical layout and raw order of words in the page
+        td = poppler.TextOutputDev(None, True, True, False)
         self._doc.displayPage(td, page_nr, 72, 72, 0, True, True, False)
         text_page = td.takeText()
         # use physical layout to make word list
@@ -428,7 +428,7 @@ class PdfProcessor(DocumentProcessor):
         line_start_x = -1 # horizontal position of beginning of the line
         words_text = [] # list of words in current line
         import sys
-        prev = {'y2':-1}
+        prev = {'x1':-1,'y1':-1,'x2':-1,'y2':-1}
         x1 = y1 = x2 = y2 = 0
         for i in xrange(words.getLength()):
 
@@ -459,6 +459,20 @@ class PdfProcessor(DocumentProcessor):
                 # keep line start, used to compute line width
                 line_start_x = x1
 
+            # NOTE: check if word of current line is coherent with previous one
+            # if previous word is further to the right than the current, 
+            # it's probably wrong, remove it and redo line from scratch
+            # this can typically happen on the first line of a page, where
+            # 'rogue' elements such as page number in footer appear at
+            # the beginning of the word list
+            if (prev['x2'] > x1):
+                #self.logger.debug('--> redo line')
+                words_text = []
+                line['x'] = [] 
+                line_start_x = x1
+                line['t'] = y1
+                line['l'] = x1
+                line['h'] = abs(y2-y1)
 
             # store left and right coordinates of word
             line['x'].append({'l':x1,'r':x2})

commit 31aa8a354492132e3d10d1a0161bef688cadac77

Improved detection of rogue words on page start
* commit 6df4e8e4c5eb7738ab04 broke selection
  for some files, this is now fixed

multivio/pdf_processor.py
@@ -440,7 +440,7 @@ class PdfProcessor(DocumentProcessor):
             #self.logger.debug("new word [%s], coord:[%s,%s/%s,%s]"\
             # %(w.getText(), x1,y1,x2,y2))
 
-            # detect new line based on line height
+            # detect new line based on height
             if (y2 > prev['y2']):
                 if (line is not None):
                     # separate each word by a space
@@ -460,12 +460,21 @@ class PdfProcessor(DocumentProcessor):
                 line_start_x = x1
 
             # NOTE: check if word of current line is coherent with previous one
-            # if previous word is further to the right than the current, 
+            # if previous word is further down in the page than the current, 
             # it's probably wrong, remove it and redo line from scratch
             # this can typically happen on the first line of a page, where
             # 'rogue' elements such as page number in footer appear at
             # the beginning of the word list
-            if (prev['x2'] > x1):
+            # compute height difference
+            hdiff = prev['y1'] - y1
+            #if (i < 50):
+            #    self.logger.debug('===> word: [%s], prev: %s'%(w.getText(),prev))
+            #    self.logger.debug('===> hdiff: %s'%(hdiff))
+
+            # difference should be zero in normal cases
+            # if difference more than 10% of page height,
+            # it's either a rogue word or first word on a new line
+            if (hdiff > 0.1 * page['h']):
                 #self.logger.debug('--> redo line')
                 words_text = []
                 line['x'] = [] 
@@ -473,9 +482,9 @@ class PdfProcessor(DocumentProcessor):
                 line['t'] = y1
                 line['l'] = x1
                 line['h'] = abs(y2-y1)
-
+            
             # store left and right coordinates of word
-            line['x'].append({'l':x1,'r':x2})
+            line['x'].append({'l':x1,'r':x2,'TODO': w.getText()})
             # store word in text list
             words_text.append(w.getText())
 